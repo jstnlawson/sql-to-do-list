39cad40144cc21648a2d7c8a89fef76428cdeecd
Justin Lawson <jstn.lawson@gmail.com>
2023-06-24 09:12:00 -0500

    wrote get on router and client sides, it's working, also wrote render function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,42 @@
 $(document).ready(onReady)
 
 function onReady() {
-    
+getTodos()
 }
+
+// GET request to put todos from database onto DOM
+function getTodos() {
+    console.log("in getTodos");
+    // ajax call to server to get koalas
+    $.ajax({
+      type: "GET",
+      url: "/todo",
+    })
+      .then((response) => {
+        renderTodos(response);
+      })
+      .catch((error) => {
+        console.log("error in get client", error);
+      });
+  } // end getTodos
+
+
+let todoObject = {
+    task: $('#task-input').val()
+}
+
+function renderTodos(todos) {
+    $("#task-box").empty();
+  
+    for (let i = 0; i < todos.length; i += 1) {
+      let todo = todos[i];
+      let newRow = $(`
+      <tr data-id = ${todo.id}>
+      <td>${todo.task}</td>
+      <td><button class = "complete-btn">✔</button></td>
+      <td><button class = "delete-btn">❌</button></td>
+      </tr>
+    `);
+    $("#task-box").append(newRow);
+    }
+  }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,6 +2,17 @@ const express = require('express');
 const todoRouter = express.Router();
 const pool = require('../modules/pool')
 
+//get
+todoRouter.get('/', (req, res) => {
 
+    let queryText = 'SELECT "task" FROM "todo"'
+    pool.query(queryText).then(results => {
+        res.send(results.rows)
+    })
+    .catch(error => {
+        console.log('error getting todo data', error)
+        res.sendStatus(500)
+    })
+});
 
 module.exports = todoRouter;
