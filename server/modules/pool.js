6f4d353db19a6b199a33b8ae644ffdc676aa35f8
Justin Lawson <jstn.lawson@gmail.com>
2023-06-26 15:44:27 -0500

    updated pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,22 @@
 const pg = require('pg');
 
+let pool;
 // Setting up pg to connect to the database
 // Creating todo database pool
-const pool = new pg.Pool({
+if (process.env.DATABASE_URL){
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+} else {
+    pool = new pg.Pool({
     database: 'weekend_to_do_app',
     host: 'localhost',
     port: 5432
-});
+})
+};
 
 // Exporting pool for use in server
 module.exports = pool
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+//env. = environment
+//use magic or local5000
 const PORT = process.env.PORT || 5000;
 const todoRouter = require('./routes/todo.router.js')
 
